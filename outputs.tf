0b532080e85921c22147b8f90764fffbab90494d
Gerd Koenig <koenig.bodensee@googlemail.com>
2019-10-04 14:16:46 +0200

    added lambda function arn as output

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,7 @@ output "aws_account_id" {
 output "aws_caller_user" {
   value = "${data.aws_caller_identity.current.user_id}"
 }
+
+output "lambda_function_arn" {
+  value = "${aws_lambda_function.lambda_ec2_terminator.arn}"
+}
